475e697e60f40db8d31734dceadda78e6d7e562a
Daniel Lamas <dlamas1298@gmail.com>
2025-01-22 20:08:21 -0300

    fix: make duration optional in AdvancedWaypointActivityModel

--- src/app/(private)/(internal)/personal/optimization/_models/advanced-waypoint-activity.model.ts
+++ src/app/(private)/(internal)/personal/optimization/_models/advanced-waypoint-activity.model.ts
@@ -2,5 +2,5 @@ import { ActivityModel } from '@/common/models'
 
 export interface AdvancedWaypointActivityModel
 	extends Partial<Omit<ActivityModel, 'duration'>> {
-	duration: string
+	duration?: string
 }
